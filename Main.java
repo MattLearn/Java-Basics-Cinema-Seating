9e29f6cc9664feba992294ecb5da8fef0641f6d4
Matthew B <31496100+MattLearn@users.noreply.github.com>
2023-12-15 06:23:32 -0500

    Update Main.java
    - Refactored variable and method names
    - Improved readability of methods
    - Corrected mistake in for loops in displaySeating

--- Main.java
+++ Main.java
@@ -2,53 +2,54 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-static char[][] display;
-static int totalSold;
+static char[][] theatreGrid;
+static int ticketsSold;
 static final int under60Price = 10;
 static final int over60Price = 8;
 
-    public static void initializeSeating(int rows, int seats){
-        display = new char[rows][seats];
-        for(char[] seat: display){
-            Arrays.fill(seat,'S');
+    private static void initializeSeating(int rows, int seats){
+        theatreGrid = new char[rows][seats];
+        for(char[] row: theatreGrid){
+            Arrays.fill(row,'S');
         }
     }
-    public static void displaySeating(int rows, int seats){
+    private static void displaySeating(){
         System.out.println("Cinema:");
-        for (int seatCol=0; seatCol < seats; seatCol++){
-            if (seatCol < 1){
+        for (int seatNum = 0; seatNum < theatreGrid[0].length; seatNum++){
+            if (seatNum < 1){
                 System.out.print("  ");
             }
-            System.out.print(seatCol+1 + " ");
+            System.out.print(seatNum+1 + " ");
         }
 
         System.out.println();
 
-        for (int seating = 0; seating < rows; seating++) {
-            for (int row = 0; row < seats; row++) {
-                if (row == 0){
-                    System.out.print(seating+1 + " ");
+        for (int row = 0; row < theatreGrid.length; row++) {
+            for (int seat = 0; seat < theatreGrid[0].length; seat++) {
+                if (seat == 0) {
+                    System.out.print((row + 1) + " ");
                 }
-                System.out.print(display[seating][row] + " ");
+                System.out.print(theatreGrid[row][seat] + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 
-    public static Boolean bookSeating(int reservation_seat, int reservation_row){
-        if(display[reservation_seat-1][reservation_row-1] == 'B'){
+    private static Boolean bookSeating(int reservedSeat, int reservedRow){
+        if(theatreGrid[reservedSeat-1][reservedRow-1] == 'B'){
             return false;
         }
         else{
-            display[reservation_seat-1][reservation_row-1] = 'B';
+            theatreGrid[reservedSeat-1][reservedRow-1] = 'B';
             return true;
         }
     }
 
-    public static int soldSeating(){
+    private static int countSeatsSold(){
         int count = 0;
-        for (char[] chars : display) {
-            for (int seats = 0; seats < display[0].length; seats++) {
+        for (char[] chars : theatreGrid) {
+            for (int seats = 0; seats < theatreGrid[0].length; seats++) {
                 if (chars[seats] == 'B') {
                     count++;
                 }
@@ -56,48 +57,48 @@ static final int over60Price = 8;
         }
         return count;
     }
-    public static void theatreStats(){
-        System.out.println("Number of purchased tickets: "+soldSeating());
-        float percentage = (float) soldSeating() / (display.length * display[0].length) * 100;
+    private static void printStats(){
+        System.out.println("Number of purchased tickets: "+ countSeatsSold());
+        float percentage = (float) countSeatsSold() / (theatreGrid.length * theatreGrid[0].length) * 100;
         String format = String.format("%.02f", percentage);
         System.out.println("Percentage: " + Float.valueOf(format) + "%");
-        System.out.println("Current income: $"+totalSold);
-        System.out.println("Total income: $"+totalIncome());
+        System.out.println("Current income: $"+ ticketsSold);
+        System.out.println("Total income: $"+ calculateTotalIncome());
     }
 
-    public static int totalIncome(){
-        if(display.length * display[0].length > 60){
-            return (display.length*display[0].length*under60Price);
+    private static int calculateTotalIncome(){
+        if(theatreGrid.length * theatreGrid[0].length > 60){
+            return (theatreGrid.length* theatreGrid[0].length*under60Price);
         }
         else{
-            return((((display.length/2)*display[0].length)*10)+(((display.length-(display.length/2))*display[0].length)*8));
+            return((((theatreGrid.length/2)* theatreGrid[0].length)*10)+(((theatreGrid.length-(theatreGrid.length/2))* theatreGrid[0].length)*8));
         }
     }
 
-    public static void ticketCost(int row){
-        if(display.length * display[0].length > 60){
-            if(row > display.length/2){
+    private static void calculateTicketCost(int row){
+        if(theatreGrid.length * theatreGrid[0].length > 60){
+            if(row > theatreGrid.length/2){
                 System.out.println("Ticket price: $" + over60Price);
-                totalSold +=over60Price;
+                ticketsSold +=over60Price;
             }
             else{
                 System.out.println("Ticket price: $" + under60Price);
-                totalSold += under60Price;
+                ticketsSold += under60Price;
             }
         }
         else{
             System.out.println("Ticket price: $" + under60Price);
-            totalSold += under60Price;
+            ticketsSold += under60Price;
         }
     }
 
-    public static void setReservation(){
+    private static void setReservation(){
         Scanner myReader = new Scanner(System.in);
         System.out.println("Enter row number:");
         int booking_row = myReader.nextInt();
         System.out.println("Enter a seat number in that row:");
         int booking_seat = myReader.nextInt();
-        if(booking_row > display.length || booking_seat > display[0].length){
+        if(booking_row > theatreGrid.length || booking_seat > theatreGrid[0].length){
             System.out.println("Wrong input!");
             setReservation();
         }
@@ -107,7 +108,7 @@ static final int over60Price = 8;
                 setReservation();
             }
             else{
-                ticketCost(booking_row);
+                calculateTicketCost(booking_row);
             }
         }
     }
@@ -115,24 +116,23 @@ static final int over60Price = 8;
     public static void main(String[] args) {
         int choice;
         Scanner myReader = new Scanner(System.in);
-        // set size of theatre
         System.out.println("Enter number of rows:");
         int rows = myReader.nextInt();
         System.out.println("Enter number of seats in each row:");
         int seats = myReader.nextInt();
         initializeSeating(rows, seats);
-        displaySeating(rows, seats);
+        displaySeating();
         do {
             System.out.println("1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit");
             choice = myReader.nextInt();
             if(choice == 1){
-                displaySeating(rows, seats);
+                displaySeating();
             }
             if(choice == 2){
                 setReservation();
             }
             if(choice == 3){
-                theatreStats();
+                printStats();
             }
         } while(choice != 0);
     }
